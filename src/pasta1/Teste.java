e0255410fc39b6326107910dee3a12ed49b1ad24
Eduardo9345 <119677357+Eduardo9345@users.noreply.github.com>
2022-12-02 12:00:25 -0300

    Blah

--- src/pasta1/Teste.java
+++ src/pasta1/Teste.java
@@ -1,10 +1,10 @@
 package pasta1;
 
-public class Teste {
+import javax.swing.JOptionPane;
 
+public class Teste {
 	public static void main(String[] args) {
-		
 		System.out.println("Hello World!");
-		
+		JOptionPane.showMessageDialog(null, "Blah Blah");
 	}
 }
