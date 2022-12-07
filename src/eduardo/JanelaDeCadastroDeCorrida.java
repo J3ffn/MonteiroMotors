d7c156b3e61e9a52d0a978f09b3fb0fe2aa3fded
Eduardo9345 <119677357+Eduardo9345@users.noreply.github.com>
2022-12-07 14:59:08 -0300

    Ajustes

--- src/eduardo/JanelaDeCadastroDeCorrida.java
+++ src/eduardo/JanelaDeCadastroDeCorrida.java
@@ -1,34 +1,90 @@
 package eduardo;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.ParseException;
+import java.time.LocalDate;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.text.MaskFormatter;
 
+import ListaDeAquecimento.CentralDeInformacoes;
+import ListaDeAquecimento.Corrida;
+import ListaDeAquecimento.Passageiro;
+import ListaDeAquecimento.Persistencia;
 import clebson.JanelaPadrao;
 
 public class JanelaDeCadastroDeCorrida extends JanelaPadrao{
+	CentralDeInformacoes central;
+	Persistencia persistencia;
+	Passageiro passageiro;
+	
 	JTextField inputEnderecoPartida;
 	JTextField inputEnderecoDestino;
 	JFormattedTextField inputDistancia;
 	JFormattedTextField inputDataDaCorrida;
+	JCheckBox checkBCorridaParaAgora;
 	
-	public JanelaDeCadastroDeCorrida() {
+	public JanelaDeCadastroDeCorrida(CentralDeInformacoes c, Persistencia per, Passageiro p) {
 		super("Cadastrar Solicitação de Corrida");
 		this.adicionarBotoes();
 		this.adicionarTextos();
 		this.setSize(500, 320);
+		central = c;
+		passageiro = p;
+		persistencia = per;
 		this.setVisible(true);
 	}
 	public void adicionarBotoes() {
 		JButton botaoConfirmar = new JButton("Confirmar");
 		botaoConfirmar.setBounds(360, 240, 100, 30);
-		
+		botaoConfirmar.addActionListener(new OuvinteDoBotaoConfirmarCadastroCorrida(this));
 		this.add(botaoConfirmar);
+	}
+	public class OuvinteDoBotaoConfirmarCadastroCorrida implements ActionListener{
+		private JanelaDeCadastroDeCorrida janela;
+		public OuvinteDoBotaoConfirmarCadastroCorrida(JanelaDeCadastroDeCorrida j) {
+			janela = j;
+		}
+			public void actionPerformed(ActionEvent e) {
+				if(!(inputEnderecoPartida.getText().equals("") || inputEnderecoDestino.getText().equals("") || inputDistancia.getText().equals(""))) {
+					LocalDate dataDaCorrida = null;
+					
+					if(!checkBCorridaParaAgora.isSelected()) {
+						String[] datas = inputDataDaCorrida.getText().split("/");
+						dataDaCorrida = LocalDate.of(Integer.parseInt(datas[2]), 
+								Integer.parseInt(datas[1]), 
+								Integer.parseInt(datas[0]));
+					} else {
+						dataDaCorrida = LocalDate.now();
+					}
+					Corrida c = new Corrida(inputEnderecoPartida.getText(), 
+							inputEnderecoDestino.getText(), 
+							Float.parseFloat(inputDistancia.getText()), 
+							checkBCorridaParaAgora.isSelected(), 
+							dataDaCorrida, 
+							passageiro);
+					central.adicionarCorrida(c);
+					
+					try {
+						persistencia.salvar(central, "dados-passageiros.xml");
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+					JOptionPane.showMessageDialog(janela, "Corrida adicionada com Sucesso!");
+					janela.dispose();
+				} else {
+					JOptionPane.showMessageDialog(janela, "Preencha os campos corretamente", "ERRO!", JOptionPane.ERROR_MESSAGE);
+				}
+			}
 	}
 	public void adicionarTextos() {
 		JLabel txTitulo = new JLabel("Cadastrar Solicitação de Corrida");
@@ -47,21 +103,41 @@ public class JanelaDeCadastroDeCorrida extends JanelaPadrao{
 		inputEnderecoDestino = new JTextField();
 		inputEnderecoDestino.setBounds(30, 140, 428, 30);
 		
-		JLabel txDistancia = new JLabel("Distancia (Km):");
+		JLabel txDistancia = new JLabel("Distancia (0000.00 Km):");
 		txDistancia.setBounds(30, 170, 134, 20);
-		inputDistancia = new JFormattedTextField();
+		try {
+			inputDistancia = new JFormattedTextField(new MaskFormatter("####.##"));
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		inputDistancia.addKeyListener(new OuvinteDoTecladoParaApenasNumerico());
-		inputDistancia.setBounds(30, 190, 70, 30);
+		inputDistancia.setBounds(30, 190, 134, 30);
 		
 		JLabel txDataDaCorrida = new JLabel("Data da Corrida: ");
 		MaskFormatter msk = null;
-		txDataDaCorrida.setBounds(169, 166, 100, 30);
+		txDataDaCorrida.setBounds(175, 166, 100, 30);
 		try {
 			msk = new MaskFormatter("##/##/####");
 		} catch (ParseException e) {}
 		inputDataDaCorrida = new JFormattedTextField(msk);
 		inputDataDaCorrida.setHorizontalAlignment(JTextField.CENTER);
-		inputDataDaCorrida.setBounds(169, 190, 70, 30);
+		inputDataDaCorrida.setBounds(175, 192, 70, 30);
+		
+		checkBCorridaParaAgora = new JCheckBox("Corrida para agora!", false);
+		checkBCorridaParaAgora.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(checkBCorridaParaAgora.isSelected()) {
+					inputDataDaCorrida.setEnabled(false);
+				} else {
+					inputDataDaCorrida.setEnabled(true);
+				}
+			}
+			
+		});
+		checkBCorridaParaAgora.setBounds(275, 187, 170, 30);
 		
 		this.add(txTitulo);
 		this.add(txDistancia);
@@ -72,5 +148,6 @@ public class JanelaDeCadastroDeCorrida extends JanelaPadrao{
 		this.add(inputDistancia);
 		this.add(inputEnderecoDestino);
 		this.add(inputEnderecoPartida);
+		this.add(checkBCorridaParaAgora);
 	}
 }
--- src/eduardo/Teste2.java
+++ src/eduardo/Teste2.java
@@ -7,6 +7,7 @@ import ListaDeAquecimento.Administrador;
 import ListaDeAquecimento.CentralDeInformacoes;
 import ListaDeAquecimento.Corrida;
 import ListaDeAquecimento.Mototaxista;
+import ListaDeAquecimento.Passageiro;
 import ListaDeAquecimento.Persistencia;
 
 public class Teste2 {
@@ -24,11 +25,11 @@ public class Teste2 {
 		
 		System.out.println(central.getTodosOsUsuarios());
 		LocalDate data = LocalDate.now();
-		Mototaxista n = new Mototaxista("Eduardo", "Masculino", "eduardo", "kkk", null);
+		Passageiro n = new Passageiro("Eduardo", "Masculino", "eduardo", "kkk", null);
 		Corrida c = new Corrida("Bla", "Bla", 60f, true, data , new Administrador("Eduardo", "Masculino", "edd", "kkk", null));
 		
 		//n.adicionarCreditos(2, central);
 		//JanelaDeReividicacaoDeCorrida janela2 = new JanelaDeReividicacaoDeCorrida(central, c, n);
-		JanelaDeCadastroDeCorrida janela5 = new JanelaDeCadastroDeCorrida();
+		JanelaDeCadastroDeCorrida janela5 = new JanelaDeCadastroDeCorrida(central, per, n);
 	}
 }
